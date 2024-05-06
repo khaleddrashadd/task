0580e2c8339984b3fb8bdf188df70b1f2d043979
Khaled Rashad <khaledrashadd@outlook.com>
2024-05-06 19:51:35 +0300

    enhanced menu look

--- src/components/mainLayout/menu/MenuItem.vue
+++ src/components/mainLayout/menu/MenuItem.vue
@@ -1,19 +1,17 @@
 <template>
-  <div
-    :='$attrs'
-    class="cursor-pointer flex items-center gap-3 px-1 py-4"
-  >
+  <div :="$attrs" class="cursor-pointer flex items-center gap-3 px-1 py-4">
     <div v-if="icon" class="flex items-center justify-center w-[18px] aspect-square">
       <img :src="icon" alt="icon" />
     </div>
-    <span>{{title}}</span>
+    <span v-if="!$slots.default">{{ title }}</span>
+    <div v-else><slot /></div>
   </div>
 </template>
 
 <script setup>
 defineOptions({
-  inheritAttrs: false,
-})
+  inheritAttrs: false
+});
 defineProps(['title', 'icon']);
 </script>
 
--- src/components/mainLayout/nav/NavMenu.vue
+++ src/components/mainLayout/nav/NavMenu.vue
@@ -1,16 +1,14 @@
 <template>
   <BaseCard class="min-w-[252px]">
-    <MenuItem
-      class="border-b-[0.5px] border-b-main/10"
-      title="Company Profile"
-      @click="() => $router.push('/company-profile')"
-    />
-    <MenuItem
-      class="border-b-[0.5px] border-b-main/10"
-      title="Book a Ride"
-      @click="() => $router.push('/')"
-    />
-    <MenuItem title="Rides" @click="() => $router.push('/rides')" />
+    <MenuItem class="border-b-[0.5px] border-b-main/10">
+      <router-link to="/company-profile">Company Profile</router-link>
+    </MenuItem>
+    <MenuItem class="border-b-[0.5px] border-b-main/10">
+      <router-link to="/">Book a Ride</router-link>
+    </MenuItem>
+    <MenuItem>
+      <router-link to="/rides">Rides</router-link>
+    </MenuItem>
   </BaseCard>
 </template>
 
